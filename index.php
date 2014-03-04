ab1a6c0e5ae3c3b5e1e52b76d85b916f1bc0741a
Wakkos <wakkos@gmail.com>
2014-03-04 19:04:38 +0100

    IHNIWIM

--- index.php
+++ index.php
@@ -4,8 +4,16 @@
 	<meta charset="UTF-8">
 	<title><?php bloginfo("description"); ?></title>
 	<link rel="stylesheet" href="<?php bloginfo("stylesheet_url"); ?>">
+
+	<!-- Comienza el WP_HEAD -->
+	<?php wp_head(); ?>
+	<!-- ACABA EL WP_HEAD -->
 </head>
 <body>
+<a class="twitter-timeline" href="https://twitter.com/Wakkos" data-widget-id="439530166520270848">Tweets por @Wakkos</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+
 	<p>Nombre del Sitio: <?php bloginfo("name"); ?></p>
 	<p>Descripción del Sitio: <?php bloginfo("description"); ?></p>
 	<p>Versión:  <strong><?php bloginfo("name"); ?></strong></p>
