14c22df12dec0583b71a9e0b7f4edde4ffe3e069
Wakkos <wakkos@gmail.com>
2014-03-07 22:45:32 +0100

    IHNIWID

--- index.php
+++ index.php
@@ -1,71 +1,30 @@
 <?php get_header(); ?>
 <body>
 	<!-- LOOP 1 -->
-	<?php rewind_posts(); ?>
-		
-		<?php 
-		
-		if ( is_home() ) {
-			query_posts( 'cat=-2' );
-		} 
-		?>
 
-		<?php if ( have_posts() ) {
+<?php
+      $args = array('post_type' => 'attachment', 'numberposts' => -1);
+       $attachments = get_posts($args);
+      if ($attachments) {
+        foreach ($attachments as $attachment) {
+          $post_id = $attachment->post_parent;
+          echo "<li>" .wp_get_attachment_image($attachment->ID, ''). "</li>";
+         }
+      }
+    ?>
 
-			while ( have_posts() ) : the_post(); ?>
-
-				<!-- post -->
-				<article>
-					<header><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></header>
-					<?php //the_excerpt() ?>
-					<?php the_date('d \d\e F , Y') ?> <?php the_time() ?> por <?php the_author() ?>
-					<?php the_content(" Más! "); ?>
-
-					<p></p>
-					<p></p>
-					<p><?php the_excerpt(); ?></p>
-
-					<p><?php the_content();?></p>
-				</article>
-
-			<?php endwhile; ?>
-
-			<!-- post navigation -->
-
-		<?php }else{ ?>
-			<p>NO hay posts en el LOOP 1</p>
-			<!-- no posts found -->
-		<?php } ?>
-
-			<hr>
+<?php
+      $args = array('post_type' => 'attachment', 'numberposts' => -1);
+       $attachments = get_posts($args);
+      if ($attachments) {
+        foreach ($attachments as $attachment) {
+          $post_id = $attachment->post_parent;
+          echo "<li>" .wp_get_attachment_url($attachment->ID, ''). "</li>";
+         }
+      }
+    ?>
+    
 		<!-- LOOP 2 -->
-		<?php rewind_posts(); ?>
-		<?php query_posts("cat=1,2,3"); ?>
-	<?php if ( have_posts() ) {
-
-			while ( have_posts() ) : the_post(); ?>
-
-				<!-- post -->
-				<article>
-					<header><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></header>
-					<?php //the_excerpt() ?>
-					<?php the_date('d \d\e F , Y') ?> <?php the_time() ?> por <?php the_author() ?>
-					<?php the_content(" Más! "); ?>
-
-					<p></p>
-					<p></p>
-					<p><?php the_excerpt(); ?></p>
-
-					<p><?php the_content();?></p>
-				</article>
-
-			<?php endwhile; ?>
-
-			<!-- post navigation -->
-
-		<?php }else{ ?>
-			<p>NO hay posts</p>
-			<!-- no posts found -->
-		<?php } ?>
+		
 
 <?php get_footer(); ?>
