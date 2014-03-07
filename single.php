56b97a372951f197fc7d5f8fb16edc831b9e047e
doblas <rafaeldoblasj@gmail.com>
2014-03-07 23:55:22 +0100

    Update single.php

--- single.php
+++ single.php
@@ -3,7 +3,21 @@
 	
 <main class="single">
 		
-		<!-- Mete tu contenido -->
+	<?php the_post(); ?>
+ 
+        <article>
+ 			<!-- Aqui saco el titulo del post -->
+            <h1><?php the_title(); ?></h1>
+			<!-- Aqui saco el autor y la fecha de publicacion del post -->
+                <small>
+                    Por <?php the_author(', '); ?> el <?php the_time('F jS, Y'); ?> en <?php the_category(', ') ?> | <?php comments_popup_link('No Comments »', '1 Comment »', '% Comments »'); ?>
+                </small>
+ 			<!-- Aqui el contenido del post -->
+            <p>
+                <?php the_content(); ?>
+            </p>
+ 
+        </article>
 
 </main>
 
